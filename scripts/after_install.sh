27a29520e428cbe423615d5ef36fc11e75c9883e
Anirudh Duggal <anirudhdggl@gmail.com>
2021-10-30 15:17:43 +0530

    TG-251: Added deploy steps (#2)
    Changes done to the following files
    - appspec.yml
    - before_install.sh
    - after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-sudo mv /home/erpuser/tag-workflow/* /home/erpuser/tag_workflow/
-sudo mv /home/erpuser/tag-workflow/.* /home/erpuser/tag_workflow/
-
+ls /home/erpuser
+mkdir /home/erpuser/frappe-bench/apps/tag_workflow/
+mv /home/erpuser/tag-workflow/* /home/erpuser/frappe-bench/apps/tag_workflow/
+mv /home/erpuser/tag-workflow/.* /home/erpuser/frappe-bench/apps/tag_workflow/
 cd /home/erpuser/frappe-bench/apps && ls
-python3 -m pip install -U -e tag_workflow
+sudo chown -R erpuser:erpuser /home/erpuser/frappe-bench/apps/tag_workflow/
+sudo python3 -m pip install -U -e tag_workflow
 bench update
 bench setup requirements
+sudo rm /home/erpuser/frappe-bench/sites/apps.txt
+touch /home/erpuser/frappe-bench/sites/apps.txt
+echo -e "frappe\nerpnext\ntag_workflow" >> /home/erpuser/frappe-bench/sites/apps.txt
+chmod 644 /home/erpuser/frappe-bench/sites/apps.txt
+cat /home/erpuser/frappe-bench/sites/apps.txt
+ls -l /home/erpuser/frappe-bench/sites/
 bench build --app tag_workflow
+sudo supervisorctl restart all
+bench --site site1.local install-app tag_workflow
+sudo apt-get install jq -y
+echo $(jq '.maintenance_mode |= 0' /home/erpuser/frappe-bench/sites/common_site_config.json) > /home/erpuser/frappe-bench/sites/common_site_config.json
 sudo supervisorctl restart all
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cd /home/erpuser/frappe-bench/apps
+bench uninstall-app tag_workflow -y
 sudo rm -rf /home/erpuser/frappe-bench/apps/tag_workflow
