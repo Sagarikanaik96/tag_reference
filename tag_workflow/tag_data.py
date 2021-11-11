d5382992e770d70daafce22441a1b5e8a5682e59
kanchankorecent <kanchan@korecent.com>
2021-11-11 20:29:09 +0530

    employee assignment

--- tag_workflow/tag_data.py
+++ tag_workflow/tag_data.py
@@ -21,7 +21,7 @@ def assign_quotation(quotation_name=None, job_order_name=None):
     return "success"
 @frappe.whitelist()
 
-def submit_values(job_order_name=None):
+def submit_values(job_order_name=None,quotation_number=None):
     print(job_order_name)
     q = frappe.db.sql(
         """ select docstatus from `tabQuotation` where job_order='{}'; """.format(job_order_name),as_list=1)
@@ -30,8 +30,18 @@ def submit_values(job_order_name=None):
         if q[i][0]==1:
             print(q[i][0])
             break
-    else:
+    else:        
         return "success"
+@frappe.whitelist()
+def update_job_order(job_order_name=None,quotation_name=None):
+    print(job_order_name,quotation_name)
+    d=frappe.get_doc("Job Order",job_order_name)
+    print(d)
+    print(d.no_of_workers)
+    d.quotation_submitted=quotation_name
+    d.save()
 
+    return "success"
+    
 
 
