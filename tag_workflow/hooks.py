b62194cf438a0ca31c7fcf135ad610a29fc357c3
sahil-sf <sahil.saini@sourcefuse.com>
2021-11-17 19:23:45 +0530

    tag js file update

--- tag_workflow/hooks.py
+++ tag_workflow/hooks.py
@@ -9,21 +9,13 @@ app_color = "grey"
 app_email = "shadab.sutar@sourcefuse.com"
 app_license = "MIT"
 app_logo_url = "/assets/tag_workflow/images/TAG-Logo-Emblem.png"
+
 # Includes in <head>
 # ------------------
 fixtures = ["Workspace", "Website Settings", "Web Page"]
 on_session_creation = [
         "tag_workflow.utils.trigger_session.on_session_creation"
 ]
-
-# include js, css files in header of desk.html
-# app_include_css = "/assets/tag_workflow/css/tag_workflow.css"
-
-# include js, css files in header of web template
-# web_include_css = "/assets/tag_workflow/css/tag_workflow.css"
-# web_include_js = "/assets/tag_workflow/js/tag_workflow.js"
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "tag_workflow/public/scss/website"
 
 app_include_css = [
         "/assets/tag_workflow/css/tag.css"
