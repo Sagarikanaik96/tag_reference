f85192f9e4e9bb016daf2b841f35cecdbe409c99
kanchan-sf <93128824+kanchan-sf@users.noreply.github.com>
2021-11-17 18:03:26 +0530

    Delete tag.js

--- tag_workflow/public/js/tag.js
+++
@@ -1,7 +0,0 @@
-frappe.provide("frappe.toolbar");
-frappe.provide("tag_workflow");
-
-$(document).bind('toolbar_setup', function() {
-	$(".dropdown-help").empty();
-	$('.navbar-home').html(`<img class="app-logo" src="/assets/tag_workflow/images/TAG-Logo.png">`);
-});
