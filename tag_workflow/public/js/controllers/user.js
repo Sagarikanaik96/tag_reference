1d2b713563011ac0ca332bdc27e4d1248373b3de
kanchan-sf <kanchan.sharma@sourcefuse.com>
2021-11-23 20:18:48 +0530

    TG-404:Resolve issue-Showing only hiring organization if hiring is selected in drop down and respective for staffing in user doctype

--- tag_workflow/public/js/controllers/user.js
+++ tag_workflow/public/js/controllers/user.js
@@ -34,6 +34,7 @@ frappe.ui.form.on("User", {
 	organization_type: function(frm){
 		set_options(frm);
 		init_values(frm);
+		setup_company_value(frm);
 	},
 	first_name:function(frm){
 		if(cur_frm.doc.first_name){
@@ -170,4 +171,15 @@ function setup_profile(frm){
 /*------------update employee--------------*/
 function update_employee(frm){
 	frappe.call({"method": "tag_workflow.controllers.master_controller.check_employee","args": {"name": frm.doc.name, "first_name": frm.doc.first_name, "last_name": frm.doc.last_name || '', "company": frm.doc.company, "gender": frm.doc.gender, "date_of_birth": frm.doc.birth_date, "date_of_joining": frm.doc.date_of_joining}});
+}
+
+
+function setup_company_value(frm){
+	cur_frm.fields_dict['company'].get_query = function(doc) {
+		return {
+			filters: {
+				"organization_type": cur_frm.doc.organization_type
+			}
+		}
+	}
 }
