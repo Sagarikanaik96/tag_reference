4266e9bd8dfe26054b11700221f031faed11519c
Ayushi Srivastava <84038611+sf-ayushisri@users.noreply.github.com>
2021-11-29 17:47:07 +0530

    TG-338: Removed Commented Lines and Use Variables (#87)
    * Removed a commented line.
    
    * Removed commented lines
    
    * Remove Commented Lines
    
    * Exception and unused variable
    
    * Removed commented lines

--- tag_workflow/tag_workflow/report/orders/orders.py
+++ tag_workflow/tag_workflow/report/orders/orders.py
@@ -4,6 +4,11 @@
 import frappe
 from frappe.utils import data
 
+jobOrder = 'Job Order'
+date_time = 'Date & Time'
+no_of_workers = 'No Of Workers'
+staffing_company = 'Staffing Company'
+
 def execute(filters=None):
     if not filters:
         filters={}
@@ -12,39 +17,39 @@ def execute(filters=None):
     if filters.get('status')==None:
 
         columns=[
-            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':'Job Order','width':150},
-            {'fieldname':'posting_date_time','label':('Date & Time'),'fieldtype':'Data','width':150},
+            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':jobOrder,'width':150},
+            {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':150},
             {'fieldname':'job_site','label':('Location'),'fieldtype':'Data','width':150},
-            {'fieldname':'no_of_workers','label':('No Of Workers'),'fieldtype':'Data','width':150},
+            {'fieldname':'no_of_workers','label':(no_of_workers),'fieldtype':'Data','width':150},
             {'fieldname':'bid','label':('Bids Received'),'fieldtype':'Data','width':150},
             {'fieldname':'claim','label':('Claimed By'),'fieldtype':'Data','width':150},
-            {'fieldname':'staff_org_claimed','label':('Staffing Company'),'fieldtype':'Data','width':150}
+            {'fieldname':'staff_org_claimed','label':(staffing_company),'fieldtype':'Data','width':150}
             ]
         data=frappe.db.sql(''' select name,posting_date_time,job_site,no_of_workers,bid,claim,staff_org_claimed from `tabJob Order` ''')
     elif filters.get('status')=='Completed':
         columns=[
-            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':'Job Order','width':200},
-            {'fieldname':'posting_date_time','label':('Date & Time'),'fieldtype':'Data','width':200},
+            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':jobOrder,'width':200},
+            {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':200},
             {'fieldname':'job_site','label':('Location'),'fieldtype':'Data' ,'width':200       },
-            {'fieldname':'no_of_workers','label':('No Of Workers'),'fieldtype':'Data','width':200},
-            {'fieldname':'staff_org_claimed','label':('Staffing Company'),'fieldtype':'Data','width':200}
+            {'fieldname':'no_of_workers','label':(no_of_workers),'fieldtype':'Data','width':200},
+            {'fieldname':'staff_org_claimed','label':(staffing_company),'fieldtype':'Data','width':200}
             ]
         data=frappe.db.sql(''' select name,posting_date_time,job_site,no_of_workers,staff_org_claimed from `tabJob Order` where order_status="Completed" ''')
     elif filters.get('status')=='Ongoing':
         columns=[
-            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':'Job Order','width':200},
-            {'fieldname':'posting_date_time','label':('Date & Time'),'fieldtype':'Data','width':200},
+            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':jobOrder,'width':200},
+            {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':200},
             {'fieldname':'job_site','label':('Location'),'fieldtype':'Data' ,'width':200       },
-            {'fieldname':'no_of_workers','label':('No Of Workers'),'fieldtype':'Data','width':200},
-            {'fieldname':'staff_org_claimed','label':('Staffing Company'),'fieldtype':'Data','width':200}
+            {'fieldname':'no_of_workers','label':(no_of_workers),'fieldtype':'Data','width':200},
+            {'fieldname':'staff_org_claimed','label':(staffing_company),'fieldtype':'Data','width':200}
             ]
         data=frappe.db.sql(''' select name,posting_date_time,job_site,no_of_workers,staff_org_claimed from `tabJob Order` where order_status="Ongoing" ''')
 
  
     elif filters.get('status')=='Upcoming':
         columns=[
-            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':'Job Order','width':200},
-            {'fieldname':'posting_date_time','label':('Date & Time'),'fieldtype':'Data','width':200},
+            {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':jobOrder,'width':200},
+            {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':200},
             {'fieldname':'job_site','label':('Location'),'fieldtype':'Data','width':200        },
             {'fieldname':'bid','label':('Bids Received'),'fieldtype':'Data','width':200},
             {'fieldname':'claim','label':('Claimed By'),'fieldtype':'Data','width':200},
