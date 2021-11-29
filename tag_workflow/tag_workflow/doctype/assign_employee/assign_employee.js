ea12062bb0c8aab374b53213e3f25f805cb2e8df
sahil-sf <sahil.saini@sourcefuse.com>
2021-11-29 18:21:10 +0530

    assign employee email rectification, user permission for assign employee

--- tag_workflow/tag_workflow/doctype/assign_employee/assign_employee.js
+++ tag_workflow/tag_workflow/doctype/assign_employee/assign_employee.js
@@ -3,28 +3,16 @@
 
  
 frappe.ui.form.on('Assign Employee', {
-	refresh: function(frm) {
-		if(cur_frm.doc.workflow_state=="Approved")
-		{
-			if (cur_frm.doc.modified.substring(11,19)==frappe.datetime.now_time())
-			{
-				frappe.call({
-					method:"tag_workflow.tag_data.update_job_order",
-					args:{
+	tag_status: function(frm) {
+		make_hiring_notification(frm);
+	}
+});
 
-						job_name:cur_frm.doc.job_order,
-						employee_filled:cur_frm.doc.employee_details.length,
-						staffing_org:cur_frm.doc.staffing_organization,
-						hiringorg:cur_frm.doc.hiring_organization
-					},
-
-				})
-			}
-			
-		}
-  
-	},
-	after_save:function(frm){
+/*-----------hiring notification--------------*/
+function make_hiring_notification(frm){
+	let state = cur_frm.doc.tag_status;
+	console.log(state);
+	if(state == "Approval Request"){
 		frappe.call({
 			method:"tag_workflow.tag_data.receive_hiring_notification",
 			args:{
@@ -34,9 +22,16 @@ frappe.ui.form.on('Assign Employee', {
 				emp_detail:cur_frm.doc.employee_details,
 				doc_name :cur_frm.doc.name
 			},
-			
-
-		})
+		});
+	}else if(state == "Approved"){
+		frappe.call({
+			method:"tag_workflow.tag_data.update_job_order",
+			args:{
+				job_name:cur_frm.doc.job_order,
+				employee_filled:cur_frm.doc.employee_details.length,
+				staffing_org:cur_frm.doc.staffing_organization,
+				hiringorg:cur_frm.doc.hiring_organization
+			},
+		});
 	}
- });
- 
+}
