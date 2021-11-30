970a2bc53f624e9022f38a8319adda248096ca71
sahil-sf <sahil.saini@sourcefuse.com>
2021-11-30 12:29:06 +0530

    permission and mandatory field in employee

--- tag_workflow/public/js/controllers/employee.js
+++ tag_workflow/public/js/controllers/employee.js
@@ -8,7 +8,7 @@ frappe.ui.form.on("Employee", {
 
 /*----------hide field----------*/
 function trigger_hide(frm){
-	let hide_fields = ["date_of_birth", "date_of_joining", "gender", "emergency_contact_details","salutation","erpnext_user","joining_details","job-profile","approvers_section","attendance_and_leave_details","salary_information","health_insurance_section","contact_details","sb53","personal_details","educational_qualification","previous_work_experience","history_in_company","exit", "naming_series"];
+	let hide_fields = ["date_of_birth", "date_of_joining", "gender", "emergency_contact_details","salutation","erpnext_user","joining_details","job-profile","approvers_section","attendance_and_leave_details","salary_information","health_insurance_section","contact_details","sb53","personal_details","educational_qualification","previous_work_experience","history_in_company","exit", "naming_series", "middle_name","employment_details","job_profile"];
 	for(var val in hide_fields){
 		cur_frm.toggle_display(hide_fields[val], 0);
 	}
@@ -16,7 +16,7 @@ function trigger_hide(frm){
 
 /*------required---------*/
 function required_field(frm){
-	let reqd_fields = ["email"];
+	let reqd_fields = ["email", "last_name"];
 	for(var fld in reqd_fields){
 		cur_frm.toggle_reqd(reqd_fields[fld], 1);
 	}
