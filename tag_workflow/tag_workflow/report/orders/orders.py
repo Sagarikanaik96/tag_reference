d6ba8867447e0e8c797f942f7ac10fbb8b877244
Ayushi Srivastava <84038611+sf-ayushisri@users.noreply.github.com>
2021-11-30 19:30:12 +0530

    TG-338: Replace string literals with variables (#97)
    * Removed a commented line.
    
    * Removed commented lines
    
    * Remove Commented Lines
    
    * Exception and unused variable
    
    * Removed commented lines
    
    * Use variables

--- tag_workflow/tag_workflow/report/orders/orders.py
+++ tag_workflow/tag_workflow/report/orders/orders.py
@@ -8,6 +8,8 @@ jobOrder = 'Job Order'
 date_time = 'Date & Time'
 no_of_workers = 'No Of Workers'
 staffing_company = 'Staffing Company'
+bids_received = 'Bids Received'
+claimed_by = 'Claimed By'
 
 def execute(filters=None):
     if not filters:
@@ -22,8 +24,8 @@ def execute(filters=None):
                 {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':150},
                 {'fieldname':'job_site','label':('Location'),'fieldtype':'Data','width':150},
                 {'fieldname':'no_of_workers','label':(no_of_workers),'fieldtype':'Data','width':150},
-                {'fieldname':'bid','label':('Bids Received'),'fieldtype':'Data','width':150},
-                {'fieldname':'claim','label':('Claimed By'),'fieldtype':'Data','width':150},
+                {'fieldname':'bid','label':(bids_received),'fieldtype':'Data','width':150},
+                {'fieldname':'claim','label':(claimed_by),'fieldtype':'Data','width':150},
                 {'fieldname':'staff_org_claimed','label':(staffing_company),'fieldtype':'Data','width':150}
                 ]
             data=frappe.db.sql(''' select name,posting_date_time,job_site,no_of_workers,bid,claim,staff_org_claimed from `tabJob Order` ''')
@@ -52,8 +54,8 @@ def execute(filters=None):
                 {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':jobOrder,'width':200},
                 {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':200},
                 {'fieldname':'job_site','label':('Location'),'fieldtype':'Data','width':200        },
-                {'fieldname':'bid','label':('Bids Received'),'fieldtype':'Data','width':200},
-                {'fieldname':'claim','label':('Claimed By'),'fieldtype':'Data','width':200},
+                {'fieldname':'bid','label':(bids_received),'fieldtype':'Data','width':200},
+                {'fieldname':'claim','label':(claimed_by),'fieldtype':'Data','width':200},
                 ]
             data=frappe.db.sql(''' select name,posting_date_time,job_site,bid,claim from `tabJob Order` where order_status="Upcoming" ''')
     else:
@@ -64,8 +66,8 @@ def execute(filters=None):
                 {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':150},
                 {'fieldname':'job_site','label':('Location'),'fieldtype':'Data','width':150},
                 {'fieldname':'no_of_workers','label':(no_of_workers),'fieldtype':'Data','width':150},
-                {'fieldname':'bid','label':('Bids Received'),'fieldtype':'Data','width':150},
-                {'fieldname':'claim','label':('Claimed By'),'fieldtype':'Data','width':150},
+                {'fieldname':'bid','label':(bids_received),'fieldtype':'Data','width':150},
+                {'fieldname':'claim','label':(claimed_by),'fieldtype':'Data','width':150},
                 {'fieldname':'staff_org_claimed','label':(staffing_company),'fieldtype':'Data','width':150}
                 ]
             data=frappe.db.sql(''' select name,posting_date_time,job_site,no_of_workers,bid,claim,staff_org_claimed from `tabJob Order` where company='{}' '''.format(company))
@@ -94,8 +96,8 @@ def execute(filters=None):
                 {'fieldname':'name','label':('Title'),'fieldtype':'Link','options':jobOrder,'width':200},
                 {'fieldname':'posting_date_time','label':(date_time),'fieldtype':'Data','width':200},
                 {'fieldname':'job_site','label':('Location'),'fieldtype':'Data','width':200        },
-                {'fieldname':'bid','label':('Bids Received'),'fieldtype':'Data','width':200},
-                {'fieldname':'claim','label':('Claimed By'),'fieldtype':'Data','width':200},
+                {'fieldname':'bid','label':(bids_received),'fieldtype':'Data','width':200},
+                {'fieldname':'claim','label':(claimed_by),'fieldtype':'Data','width':200},
                 ]
             data=frappe.db.sql(''' select name,posting_date_time,job_site,bid,claim from `tabJob Order` where order_status="Upcoming" AND company='{}' '''.format(company))
 
