b977735469a384da0b6f1639c42cfa5bba00fc30
sahil-sf <93121644+sahil-sf@users.noreply.github.com>
2021-12-28 16:25:55 +0530

    TG-566: developments

---
+++ tag_workflow/tag_workflow/report/employee_work_record/employee_work_record.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2016, SourceFuse and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+function get_company_list(){
+	let company = '\n';
+	frappe.call({
+		"method": "tag_workflow.utils.whitelisted.get_company_list",
+		"args": {"company_type": "Hiring"},
+		"async": 0,
+		"callback": function(r){
+			company += r.message;
+		}
+	});
+	return company
+}
+
+frappe.query_reports["Employee Work Record"] = {
+	"filters": [
+		{
+			"fieldname": "employee",
+			"label": __("Employee Code"),
+			"fieldtype": "Link",
+			"options": "Employee"
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Select",
+			"options": get_company_list(),
+		},
+	]
+};
---
+++ tag_workflow/tag_workflow/report/employee_work_record/employee_work_record.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2013, SourceFuse and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+def execute(filters=None):
+    columns, data = [], []
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    columns = [
+            {
+                "label": _("Employee Code"),
+                "fieldname": "employee",
+                "fieldtype": "Link",
+                "options": "EMployee",
+                "width": 150
+            },
+            {
+                "label": _("Employee Name"),
+                "fieldname": "employee_name",
+                "width": 150
+            },
+            {
+                "label": _("Job Title"),
+                "fieldname": "job_title",
+                "fieldtype": "Data",
+                "width": 150
+            },
+            {
+                "label": _("Start Date"),
+                "fieldname": "start_date",
+                "fieldtype": "Date",
+                "width": 150
+            },
+            {
+                "label": _("End Date"),
+                "fieldname": "end_date",
+                "fieldtype": "Date",
+                "width": 150
+            },
+            {
+                "label": _("Hours Worked"),
+                "fieldname": "hours",
+                "fieldtype": "Float",
+                "width": 150
+            },
+            {
+                "label": _("Total Payment"),
+                "fieldname": "total_payment",
+                "fieldtype": "Currency",
+                "width": 150
+            }
+        ]
+    return columns
+
+def get_condition(filters):
+    try:
+        condition = ""
+
+        if(filters.get("employee")):
+            condition += " and employee = %(employee)s"
+
+        if(filters.get("company")):
+            condition += " and company = %(company)s"
+
+        return condition
+    except Exception as e:
+        frappe.msgprint(e)
+
+
+def get_data(filters):
+    try:
+        condition = get_condition(filters)
+        data = frappe.db.sql(""" select name, job_order_detail, employee, employee_name, total_billable_hours, total_billable_amount, company from `tabTimesheet` where docstatus = 1 %s """%condition, filters, as_dict=True)
+
+        row = []
+        for d in data:
+            if(d.job_order_detail):
+                start_date, end_date, job_title = frappe.db.get_value("Job Order", {"name": d.job_order_detail}, ["from_date", "to_date", "job_title"])
+                row.append({"employee": d.employee, "employee_name": d.employee_name, "job_title": job_title, "start_date": start_date, "end_date": end_date, "job_title": job_title, "hours": d.total_billable_hours, "total_payment": d.total_billable_amount})
+
+        return row
+    except Exception as e:
+        frappe.msgprint(e)
+
