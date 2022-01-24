98cf0c8a092e6f6e7c2e07c68aeb07b668b8b5c6
sahil-sf <sahil.saini@sourcefuse.com>
2022-01-24 15:05:31 +0530

    TG-1314: fixes

--- tag_workflow/public/js/doctype_list/report_list.js
+++ tag_workflow/public/js/doctype_list/report_list.js
@@ -1,7 +1,7 @@
 frappe.listview_settings["Report"] = {
 	refresh: function (listview){
 		let roles = frappe.user_roles;
-		if(!["Tag Admin", "Tag User"].includes(roles)){
+		if(!(roles.includes("Tag Admin") || roles.includes("Tag User"))){
 			frappe.msgprint("You don't have enough permissions.");
 			frappe.set_route("app");
 		}
