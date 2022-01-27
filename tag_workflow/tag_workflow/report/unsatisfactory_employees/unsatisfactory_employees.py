9c6e7c4db070cb8df90f2ae9bb00fb40eed8a97b
kanchan-sf <kanchan.sharma@sourcefuse.com>
2022-01-27 15:29:21 +0530

    TG-1414:No data is showing to TAG admin, Staffing admin is able to see the data.

--- tag_workflow/tag_workflow/report/unsatisfactory_employees/unsatisfactory_employees.py
+++ tag_workflow/tag_workflow/report/unsatisfactory_employees/unsatisfactory_employees.py
@@ -2,12 +2,15 @@
 # For license information, please see license.txt
 
 import frappe
+from tag_workflow.tag_workflow.page.staff_company_list.staff_company_list import comp
 
 def execute(filters=None):
 	if not filters:
 		filters={}
 	columns,data=[],[]
-	company=frappe.defaults.get_user_default("Company")
+	user_name=frappe.session.user
+	sql = ''' select organization_type from `tabUser` where email='{}' '''.format(user_name)
+	user_type=frappe.db.sql(sql, as_list=1)
 	from_date=filters.get('start_date')
 	to_date=filters.get('end_date')
 
@@ -21,11 +24,9 @@ def execute(filters=None):
         {'fieldname':'from_date','label':("Start Date"),'fieldtype':'Date','width':150},
         {'fieldname':'to_date','label':('End Date'),'fieldtype':'Date','width':150}
 	]
-	if frappe.session.user=="Administrator":
+	if frappe.session.user=="Administrator" or user_type[0][0]=='TAG':
 		data=frappe.db.sql(''' select job_order_detail,employee_name,`tabJob Order`.select_job,`tabTimesheet`.company as company,total_hours,total_billable_amount,`tabJob Order`.from_date,`tabJob Order`.to_date from `tabTimesheet`,`tabJob Order` where `tabTimesheet`.job_order_detail=`tabJob Order`.name and non_satisfactory=1 and start_date>="{0}" and end_date<="{1}" '''.format(from_date,to_date))
 	else:
-		data=frappe.db.sql(''' select job_order_detail,employee_name,`tabJob Order`.select_job,`tabTimesheet`.company as company,total_hours,total_billable_amount,`tabJob Order`.from_date,`tabJob Order`.to_date from `tabTimesheet`,`tabJob Order` where `tabTimesheet`.job_order_detail=`tabJob Order`.name and employee_company='{0}' and non_satisfactory=1 and start_date>="{1}" and end_date<="{2}" '''.format(company,from_date,to_date))
-
-
+		data=frappe.db.sql(''' select job_order_detail,employee_name,`tabJob Order`.select_job,`tabTimesheet`.company as company,total_hours,total_billable_amount,`tabJob Order`.from_date,`tabJob Order`.to_date from `tabTimesheet`,`tabJob Order` where `tabTimesheet`.job_order_detail=`tabJob Order`.name and employee_company in (select company from `tabEmployee` where email ="{0}") and non_satisfactory=1 and start_date>="{1}" and end_date<="{2}" '''.format(user_name,from_date,to_date))
 
 	return columns, data
