60bcdef374ba895142ba5482d27f93603232c21b
kanchan-sf <kanchan.sharma@sourcefuse.com>
2022-02-16 17:54:46 +0530

    TG-1622:You have claimed this job order message is not showing after claiming the job order.,TG-1625

--- tag_workflow/tag_workflow/doctype/assign_employee/assign_employee.js
+++ tag_workflow/tag_workflow/doctype/assign_employee/assign_employee.js
@@ -132,6 +132,11 @@ function make_hiring_notification(frm){
 			"hiring_org" : cur_frm.doc.hiring_organization, "job_order" : cur_frm.doc.job_order,
 			"staffing_org" : cur_frm.doc.company, "emp_detail" : cur_frm.doc.employee_details, "doc_name" : cur_frm.doc.name,
 			"no_of_worker_req":frm.doc.no_of_employee_required,"is_single_share" :cur_frm.doc.is_single_share,"job_title":frm.doc.job_category
+		},
+		callback:function(r){
+			setTimeout(function () {
+				window.location.href='/app/job-order/'+frm.doc.job_order
+			}, 3000);
 		}
 	});
 }
