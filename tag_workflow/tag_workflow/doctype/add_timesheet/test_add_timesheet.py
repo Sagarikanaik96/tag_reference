735e7a8d6818deb8a6467d41890820daaa5322b7
sahil-sf <sahil.saini@sourcefuse.com>
2022-02-25 12:04:34 +0530

    TG-1682: code smell removed

--- tag_workflow/tag_workflow/doctype/add_timesheet/test_add_timesheet.py
+++ tag_workflow/tag_workflow/doctype/add_timesheet/test_add_timesheet.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2022, SourceFuse and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 import unittest
 
 class TestAddTimesheet(unittest.TestCase):
-	pass
+    pass
--- tag_workflow/tag_workflow/doctype/timesheet_item/timesheet_item.py
+++ tag_workflow/tag_workflow/doctype/timesheet_item/timesheet_item.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2022, SourceFuse and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class TimesheetItem(Document):
-	pass
+    pass
