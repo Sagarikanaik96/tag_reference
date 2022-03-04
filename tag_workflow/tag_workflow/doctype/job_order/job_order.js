c1df24de34c5d4279e636d5215105345c32608b8
kanchan-sf <kanchan.sharma@sourcefuse.com>
2022-03-04 12:33:49 +0530

    TG-1780:Need to update the preference to delete the job orders.

--- tag_workflow/tag_workflow/doctype/job_order/job_order.js
+++ tag_workflow/tag_workflow/doctype/job_order/job_order.js
@@ -1178,12 +1178,15 @@ function assigned_emp(frm){
 }
 
 function job_order_cancel_button(frm){
-	if(frm.doc.__islocal!=1 && frappe.boot.tag.tag_user_info.company_type=='Hiring' || frappe.boot.tag.tag_user_info.company_type=='Exclusive Hiring'){
-		frm.add_custom_button(__("Cancel"),function(){
-			cancel_job_order(frm);
-
-		})
-	}
+	frappe.db.get_value('User',{'name':frm.doc.owner},['organization_type'],function(r){
+		if((frm.doc.__islocal!=1 && frappe.boot.tag.tag_user_info.company_type=='Hiring' || frappe.boot.tag.tag_user_info.company_type=='Exclusive Hiring' || frappe.boot.tag.tag_user_info.company_type=='TAG') || (frm.doc.__islocal!=1 && frappe.boot.tag.tag_user_info.company_type=='Staffing' && frm.doc.company_type=='Exclusive' && r.organization_type=='Staffing')){
+			frm.add_custom_button(__("Cancel"),function(){
+				cancel_job_order(frm);
+	
+			})
+		}
+	})
+	
 }
 function cancel_job_order(frm){
 	return new Promise(function(resolve, reject) {
