b71d4072e2f4b604a9cb94383847cc11978ae688
kanchan-sf <kanchan.sharma@sourcefuse.com>
2022-03-21 15:48:52 +0530

    TG-1892:quick name search during assign emp->works when user selects the show all emp.

--- tag_workflow/tag_workflow/doctype/job_order/job_order.js
+++ tag_workflow/tag_workflow/doctype/job_order/job_order.js
@@ -16,7 +16,7 @@ frappe.ui.form.on("Job Order", {
 	},
 
 	onload: function(frm) {
-		if(frappe.boot.tag.tag_user_info.company_type=='Staffing'){
+		if(frappe.boot.tag.tag_user_info.company_type=='Staffing' && frm.doc.__islocal==1){
 			frm.set_value('e_signature_full_name', frappe.session.user_fullname);
 			frm.set_df_property("e_signature_full_name", "read_only", 1);
 		}
