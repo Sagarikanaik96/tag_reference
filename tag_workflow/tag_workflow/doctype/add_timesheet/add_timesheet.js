c8918450132ed14eeec762c096e9236ab8f0f5b6
hitesh-sf <hiteshpratap.singh@sourcefuse.com>
2022-04-11 10:05:29 +0530

    TG-2022:hiring admin->create timesheet->select the status->it will not show dropdown of status.

--- tag_workflow/tag_workflow/doctype/add_timesheet/add_timesheet.js
+++ tag_workflow/tag_workflow/doctype/add_timesheet/add_timesheet.js
@@ -278,10 +278,10 @@ function get_amount(frm, hours, breaks, child){
 		overtime_rate = cur_frm.doc.total_per_hour_rate*1.5;
 	}
 
-	frappe.model.set_value(child.doctype, child.name, "hours", total_hour.toFixed(2));
-	frappe.model.set_value(child.doctype, child.name, "working_hours", normal_hours.toFixed(2));
-	frappe.model.set_value(child.doctype, child.name, "overtime_hours", overtime_hours.toFixed(2));
-	frappe.model.set_value(child.doctype, child.name, "overtime_rate", overtime_rate.toFixed(2));
+	frappe.model.set_value(child.doctype, child.name, "hours", Math.round(total_hour * 100) / 100);
+	frappe.model.set_value(child.doctype, child.name, "working_hours", Math.round(normal_hours * 100) / 100);
+	frappe.model.set_value(child.doctype, child.name, "overtime_hours", Math.round(overtime_hours * 100) / 100);
+	frappe.model.set_value(child.doctype, child.name, "overtime_rate", Math.round(overtime_rate * 100) / 100);
 	frappe.model.set_value(child.doctype, child.name, "amount", normal_amount+overtime_amount);
 }
 
