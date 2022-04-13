6f2457db9c4edb37149e5383e1c7ac0a35881c3f
Ayushi Srivastava <84038611+sf-ayushisri@users.noreply.github.com>
2022-04-13 13:30:48 +0530

    TG-1978: CRM: Leads Page Column and Filter Changes

--- tag_workflow/public/js/doctype_list/lead_list.js
+++ tag_workflow/public/js/doctype_list/lead_list.js
@@ -1,11 +1,15 @@
 frappe.listview_settings["Lead"] = {
     onload:function(){
         if(frappe.session.user!='Administrator'){
-            $('.custom-actions.hidden-xs.hidden-md').hide()
-            $('[data-original-title="Refresh"]').hide()
-            $('.menu-btn-group').hide()
+            $('.custom-actions.hidden-xs.hidden-md').hide();
+            $('[data-original-title="Refresh"]').hide();
+            $('.menu-btn-group').hide();
         }
+        $('div[data-original-title = "Name"]').addClass('hide');
+        $('[title = "Lead"]').html('Leads');
     },
     hide_name_column: true,
-
+    refresh: ()=>{
+        $('#navbar-breadcrumbs > li:nth-child(2) > a:nth-child(1)').html('Leads');
+    }
 }
