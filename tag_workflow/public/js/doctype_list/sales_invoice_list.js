b2dd1e4f24f937cb71916ee73b128dbdebfe292f
Animesh <88422456+Animesh-12345@users.noreply.github.com>
2022-04-20 12:30:45 +0530

    TG-2064: Invoice page is having and overlapping issue of text with buttons and View Invoice button and other columns are not showing on mobile screen. (#1047)
    * TG-2064: Invoice page is having and overlapping issue of text with buttons and View Invoice button and other columns are not showing on mobile screen.
    
    * TG-2064:code smell resolved
    
    * TG-2064:Refactor code smell
    
    * TG-2064:sonar issue resolved
    
    * TG-2064:Sonar issues resolved
    
    Co-authored-by: Animesh yadav <animeshyadav@Animeshs-MacBook-Air.local>
    Co-authored-by: Animesh yadav <animeshyadav@Animeshs-Air.domain.name>

--- tag_workflow/public/js/doctype_list/sales_invoice_list.js
+++ tag_workflow/public/js/doctype_list/sales_invoice_list.js
@@ -227,4 +227,218 @@ function get_staffing_company_invoices(){
 	return frappe.flags.company
 }
 
+frappe.views.ListView.prototype.get_column_html= function(col, doc) {
+if (col.type === "Status") {
+return `
+<div class="list-row-col ellipsis">
+${this.get_indicator_html(doc)}
+</div>
+`;
+}
+if (col.type === "Tag") {
+const tags_display_class = !this.tags_shown ? 'hide' : '';
+let tags_html = doc._user_tags ? this.get_tags_html(doc._user_tags, 2) : '<div class="tags-empty">-</div>';
+return `
+<div class="list-row-col tag-col ${tags_display_class} hidden-xs ellipsis">
+${tags_html}
+</div>
+`;
+}
+return this.render_html(col,doc)
+}
 
+frappe.views.ListView.prototype.get_meta_html = function(doc) {
+let html = "";
+
+let settings_button = null;
+if (this.settings.button && this.settings.button.show(doc)) {
+settings_button = `
+<span class="list-actions">
+<button class="btn btn-action btn-default btn-xs"
+data-name="${doc.name}" data-idx="${doc._idx}"
+title="${this.settings.button.get_description(doc)}">
+${this.settings.button.get_label(doc)}
+</button>
+</span>
+`;
+}
+
+const modified = comment_when(doc.modified, true);
+
+let assigned_to = `<div class="list-assignments">
+<span class="avatar avatar-small">
+<span class="avatar-empty"></span>
+</div>`;
+
+let assigned_users = JSON.parse(doc._assign || "[]");
+if (assigned_users.length) {
+assigned_to = `<div class="list-assignments">
+${frappe.avatar_group(assigned_users, 3, { filterable: true })[0].outerHTML}
+</div>`;
+}
+
+const comment_count = `<span class="${
+!doc._comment_count ? "text-extra-muted" : ""
+} comment-count">
+${frappe.utils.icon('small-message')}
+${doc._comment_count > 99 ? "99+" : doc._comment_count}
+</span>`;
+
+html += `
+<div class="level-item list-row-activity ">
+<div class="">
+${settings_button || assigned_to}
+</div>
+${modified}
+${comment_count}
+</div>
+<div class="level-item visible-xs text-right">
+${this.get_indicator_dot(doc)}
+</div>
+`;
+
+return html;
+}
+
+frappe.views.ListView.prototype.get_header_html = function() {
+if (!this.columns) {
+return;
+}
+
+const subject_field = this.columns[0].df;
+let subject_html = `
+<input class="level-item list-check-all" type="checkbox"
+title="${__("Select All")}">
+<span class="level-item list-liked-by-me hidden-xs">
+<span title="${__("Likes")}">${frappe.utils.icon('heart', 'sm', 'like-icon')}</span>
+</span>
+<span class="level-item">${__(subject_field.label)}</span>
+`;
+const $columns = this.columns
+.map(col => {
+let classes = [
+"list-row-col ellipsis",
+col.type == "Subject" ? "list-subject level" : "",
+col.type == "Tag" ? "tag-col hide": "",
+frappe.model.is_numeric_field(col.df) ? "text-right" : "",
+].join(" ");
+
+return `
+<div class="${classes}">
+${col.type === "Subject" ? subject_html : `
+<span>${__((col.df && col.df.label) || col.type)}</span>`}
+</div>
+`;
+})
+.join("");
+
+return this.get_header_html_skeleton(
+$columns,
+'<span class="list-count"></span>'
+);
+}
+frappe.views.ListView.prototype.render_html = function(col,doc){
+const df = col.df || {};
+const label = df.label;
+const fieldname = df.fieldname;
+const value = doc[fieldname] || "";
+const format = () => {
+if (df.fieldtype === "Code") {
+return value;
+} else if (df.fieldtype === "Percent") {
+return `<div class="progress" style="margin: 0px;">
+<div class="progress-bar progress-bar-success" role="progressbar"
+aria-valuenow="${value}"
+aria-valuemin="0" aria-valuemax="100" style="width: ${Math.round(value)}%;">
+</div>
+</div>`;
+} else {
+return frappe.format(value, df, null, doc);
+}
+};
+
+const field_html = () => {
+let html;
+let _value;
+let strip_html_required =
+df.fieldtype == "Text Editor" ||
+(df.fetch_from &&
+["Text", "Small Text"].includes(df.fieldtype));
+
+if (strip_html_required) {
+_value = strip_html(value);
+} else {
+_value =
+typeof value === "string"
+? frappe.utils.escape_html(value)
+: value;
+}
+
+if (df.fieldtype === "Image") {
+html = df.options ? `<img src="${doc[df.options]}"
+style="max-height: 30px; max-width: 100%;">`
+: `<div class="missing-image small">
+${frappe.utils.icon('restriction')}
+</div>`;
+} else if (df.fieldtype === "Select") {
+html = `<span class="filterable indicator-pill ${frappe.utils.guess_colour(
+_value
+)} ellipsis"
+data-filter="${fieldname},=,${value}">
+<span class="ellipsis"> ${__(_value)} </span>
+</span>`;
+} else if (df.fieldtype === "Link") {
+html = `<a class="filterable ellipsis"
+data-filter="${fieldname},=,${value}">
+${_value}
+</a>`;
+} else if (
+["Text Editor", "Text", "Small Text", "HTML Editor", "Markdown Editor"].includes(
+df.fieldtype
+)
+) {
+html = `<span class="ellipsis">
+${_value}
+</span>`;
+} else {
+html = `<a class="filterable ellipsis"
+data-filter="${fieldname},=,${frappe.utils.escape_html(value)}">
+${format()}
+</a>`;
+}
+
+return `<span class="ellipsis"
+title="${__(label)}: ${frappe.utils.escape_html(_value)}">
+${html}
+</span>`;
+};
+
+const class_map = {
+Subject: "list-subject level",
+Field: " ",
+};
+const css_class = [
+"list-row-col ellipsis",
+class_map[col.type],
+frappe.model.is_numeric_field(df) ? "text-right" : "",
+].join(" ");
+
+const html_map = {
+Subject: this.get_subject_html(doc),
+Field: field_html(),
+};
+let column_html = html_map[col.type];
+
+// listview_setting formatter
+if (
+this.settings.formatters &&
+this.settings.formatters[fieldname]
+) {
+column_html = this.settings.formatters[fieldname](value, df, doc);
+}
+return `
+<div class="${css_class}">
+${column_html}
+</div>
+`;
+}
