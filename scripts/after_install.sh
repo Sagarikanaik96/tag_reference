4986a78344022be72166b739ec585521beb8b520
Anirudh Duggal <anirudhdggl@gmail.com>
2022-04-27 11:28:24 +0530

    TG-2182: Modified build scripts for tag-workflow (#1095)
    * TG-2182: Modified build script for tag-workflow
    
    Modified build script so that bench migrate run only once for each deployment
    and bench build runs only for the current app
    
    * Removed build commands

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,18 +8,3 @@ cp -r /home/erpuser/frappe-bench/apps/tag_workflow/ /app/frappe-bench/apps/
 
 # Ensure proper permission for code on SSD
 sudo chown -R erpuser:erpuser /app/
-
-# Build the application
-cd /app/frappe-bench/
-bench setup requirements
-bench build --app tag_workflow
-
-# Run node rollup command
-cd /app/frappe-bench/apps/tag_workflow
-node /app/frappe-bench/apps/frappe/rollup/build.js --app tag_workflow
-cd /app/frappe-bench/
-
-bench migrate
-sudo supervisorctl restart all
-echo $(jq '.maintenance_mode |= 0' /app/frappe-bench/sites/common_site_config.json) > /app/frappe-bench/sites/common_site_config.json
-sudo supervisorctl restart all
