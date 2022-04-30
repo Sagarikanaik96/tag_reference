dfcf5484783869e58056b8eabfe836a1b46d94d4
hitesh-sf <hiteshpratap.singh@sourcefuse.com>
2022-04-30 16:13:14 +0530

    TG-2239:hiring admin->create timesheet->staffing admin approve timesheet->hiring admin get notification twice.

--- tag_workflow/controllers/project_controller.py
+++ tag_workflow/controllers/project_controller.py
@@ -12,6 +12,7 @@ from frappe import enqueue
 class ProjectController(base_controller.BaseController):
     def validate_project(self):
         if(self.doc.workflow_state == "Denied" and frappe.db.get_value("User", frappe.session.user, "organization_type") not in ["Hiring", "Exclusive Hiring"]):
-            enqueue("tag_workflow.utils.timesheet.denied_notification", job_order=self.doc.job_order_detail, hiring_company=self.doc.company, staffing_company=self.doc.employee_company, timesheet_name=self.doc.name)
-        elif(self.doc.workflow_state == "Approved"):
-            enqueue("tag_workflow.utils.timesheet.approval_notification", job_order=self.doc.job_order_detail, staffing_company=self.doc.employee_company, date=None, hiring_company=self.doc.company, timesheet_name=self.doc.name, timesheet_approved_time=self.doc.modified, current_time=frappe.utils.now())
+            enqueue("tag_workflow.utils.timesheet.denied_notification", now=True,  job_order=self.doc.job_order_detail, hiring_company=self.doc.company, staffing_company=self.doc.employee_company, timesheet_name=self.doc.name)
+        elif(self.doc.workflow_state == "Approved") and self.doc.check_fun==0:
+            self.doc.check_fun=1
+            enqueue("tag_workflow.utils.timesheet.approval_notification", now=True, job_order=self.doc.job_order_detail, staffing_company=self.doc.employee_company, date=None, hiring_company=self.doc.company, timesheet_name=self.doc.name, timesheet_approved_time=self.doc.modified, current_time=frappe.utils.now())
