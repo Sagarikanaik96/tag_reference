ff86f38d4a92a0c97db10a4d7df4fe41d3e9d1e4
hitesh-sf <hiteshpratap.singh@sourcefuse.com>
2022-06-09 10:00:58 +0530

    TG-2571:Staffing/hiring side->Assign employee list-> when we click on hiring and staffing company, instead of showing a company profile, it automatically applies a filter.(sprint 13)

--- tag_workflow/tag_workflow/doctype/assign_employee/assign_employee_list.js
+++ tag_workflow/tag_workflow/doctype/assign_employee/assign_employee_list.js
@@ -8,5 +8,30 @@ frappe.listview_settings['Assign Employee'] = {
 			$('[data-original-title="Refresh"]').hide();
 			$('.menu-btn-group').hide();
         }
-    }
+    },
+
+    formatters: {
+		hiring_organization(val,f){
+            return `<span class="filterable ellipsis" title="" id="${val}-${f.name}" ><a class="filterable ellipsis" data-fieldname="${val}-${f.name}" onclick = "myfunction('${val}')">${val}</a></span>
+            <script>
+            function myfunction(name){
+                var name1= name.replace(/%/g, ' ');
+                localStorage.setItem('company', name1)
+                window.location.href= "/app/dynamic_page"
+        
+            }
+            </script>`;
+		},
+        company(val,f){
+            return `<span class="filterable ellipsis" title="" id="${val}-${f.name}" ><a class="filterable ellipsis" data-fieldname="${val}-${f.name}" onclick = "myfunction1('${val}')">${val}</a></span>
+            <script>
+            function myfunction1(name){
+                var name1= name.replace(/%/g, ' ');
+                localStorage.setItem('company', name1)
+                window.location.href= "/app/dynamic_page"
+        
+            }
+            </script>`;
+		}
+	}
 }
