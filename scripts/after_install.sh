f1c83932e64053dab88c3cd0180acbdcd59f263b
ayushgupta-98 <47833089+ayushgupta-98@users.noreply.github.com>
2022-07-22 09:43:13 +0530

    TG-3025: Update in after_install.sh to use sync

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,7 @@ sudo chown -R erpuser:erpuser /app/frappe-bench/apps/tag_workflow/
 
 # Copy code to SSD
 #cp -r /home/erpuser/frappe-bench/apps/tag_workflow/ /app/frappe-bench/apps/
-rsync -avu --delete /home/erpuser/frappe-bench/apps/frappe/ /app/frappe-bench/apps/frappe/
+rsync -avu --delete /home/erpuser/frappe-bench/apps/tag_workflow/ /app/frappe-bench/apps/tag_workflow/
 
 # Ensure proper permission for code on SSD
 sudo chown -R erpuser:erpuser /app/
