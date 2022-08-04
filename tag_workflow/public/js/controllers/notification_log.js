79e78710f7849a7f4d5021d76671cea8d7262230
nnagpure <106376793+nnagpure@users.noreply.github.com>
2022-08-04 11:23:23 +0530

    TG-3221: staffing side:- Unable to redirect to the job order page after clicking on the 'open reference document' button from notification(S16Fix) (#1647)
    * TG-3221: staffing side:- Unable to redirect to the job order page after clicking on the 'open reference document' button from notification(S16Fix)
    
    * TG:3221: Code review changes(S16Fix)

--- tag_workflow/public/js/controllers/notification_log.js
+++ tag_workflow/public/js/controllers/notification_log.js
@@ -3,20 +3,18 @@
 frappe.ui.form.off("Notification Log", "open_reference_document");
 frappe.ui.form.on('Notification Log', {
 	open_reference_document: function(frm) {
-		const dt = frm.doc.document_type;
-		const dn = frm.doc.document_name;
-        if(frappe.user_roles.includes("Staffing Admin") || frappe.user_roles.includes("Staffing User")){
-			if(dt == 'Timesheet'){
-				frappe.db.get_value("Timesheet", { name: dn},"job_order_detail", function(r1){
-					let job_order = r1.job_order_detail;
-					localStorage.setItem("order", job_order);
-					window.location.href = "/app/timesheet-approval";
-				})
-			}
-
-        }
+		const document_type = frm.doc.document_type;
+		const document_name = frm.doc.document_name;
+		let user_roles = frappe.user_roles;
+        if((document_type == 'Timesheet') && (user_roles.includes("Staffing Admin") || user_roles.includes("Staffing User"))){
+			frappe.db.get_value("Timesheet", { name: document_name},"job_order_detail", function(r1){
+				let job_order = r1.job_order_detail;
+				localStorage.setItem("order", job_order);
+				window.location.href = "/app/timesheet-approval";
+			})
+	}
         else{
-            frappe.set_route('Form', dt, dn);
+            frappe.set_route('Form', document_type, document_name);
         }
 		
 	}
