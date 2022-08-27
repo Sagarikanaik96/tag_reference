3f1460f7183ed11584b965a530a959dccd48b28c
sagarikanaik <sagarika.naik@promantia.com>
2022-08-28 02:11:30 +0530

    TG-1313: Improper Access Control

--- tag_workflow/tag_workflow/doctype/employee_assign_name/employee_assign_name.py
+++ tag_workflow/tag_workflow/doctype/employee_assign_name/employee_assign_name.py
@@ -9,7 +9,13 @@ class EmployeeAssignName(Document):
 
 @frappe.whitelist()
 def employee_email_filter(email):
+    emp_docname = frappe.db.get_value('Employee', {"user_id": email}, ['name'])
+    emp_doc = frappe.get_doc('Employee', emp_docname)
+    if not emp_doc.has_permission("read"):
+            frappe.local.response['http_status_code'] = 500
+            frappe.throw(('Insufficient Permission for User '+email))
     if frappe.session.user!="Administrator" and frappe.db.get_value("User",frappe.session.user,"role_profile_name")!="Tag Admin":
+        
         sql = """SELECT company from `tabEmployee` where user_id="{}" """.format(email)
         data = frappe.db.sql(sql, as_dict=True)
         for i in data:
