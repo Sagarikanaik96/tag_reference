9c40afd6ba6d9723b307e220dd4863189f01594a
Animesh <88422456+Animesh-12345@users.noreply.github.com>
2022-12-01 11:26:49 +0530

    TG-4913:Make all forms mobile responsive for every user type (#2217)
    * TG-4913:Make all forms mobile responsive for every user type
    
    * TG-4913: Code smell resolved
    
    Co-authored-by: Animesh yadav <animeshyadav@Animeshs-MacBook-Air.local>

--- tag_workflow/tag_workflow/page/staff_home/staff_home.js
+++ tag_workflow/tag_workflow/page/staff_home/staff_home.js
@@ -3,7 +3,7 @@ let company_type = frappe.boot.tag.tag_user_info.company_type;
 frappe.breadcrumbs.clear();
 frappe.flags.wrapper = null;
 
-frappe.pages['staff-home'].on_page_load = function(wrapper) {
+frappe.pages['staff-home'].on_page_load = function (wrapper) {
 	let page = frappe.ui.make_app_page({
 		parent: wrapper,
 		title: 'Home',
@@ -16,23 +16,23 @@ frappe.pages['staff-home'].on_page_load = function(wrapper) {
 }
 
 frappe.StaffHome = Class.extend({
-	init: function(wrapper, page) {
+	init: function (wrapper, page) {
 		let me = this;
 		this.parent = wrapper;
 		this.page = this.parent.page;
-		setTimeout(function() {
+		setTimeout(function () {
 			frappe.breadcrumbs.clear();
 			me.setup(wrapper, page);
 		}, 900);
 	},
-	setup: function(wrapper, page){
+	setup: function (wrapper, page) {
 		let me = this;
 		this.body = $('<div></div>').appendTo(this.page.main);
 		$(frappe.render_template('staff_home', "")).appendTo(this.body);
 		me.make_data(wrapper, page);
 		me.init_map(wrapper, page);
 	},
-	init_map: function(_wrapper, _page){
+	init_map: function (_wrapper, _page) {
 		let me = this;
 		let center = { lat: 38.889248, lng: -77.050636 };
 		me.map = new google.maps.Map(document.getElementById('maps'), {
@@ -40,27 +40,27 @@ frappe.StaffHome = Class.extend({
 			center: center
 		});
 	},
-	make_data: function(wrapper, page){
+	make_data: function (wrapper, page) {
 		let me = this;
 		me.update_job_order(wrapper, page);
 	},
 
-	update_job_order: function(wrapper, page){
+	update_job_order: function (wrapper, page) {
 		let me = this;
 		frappe.call({
 			method: "tag_workflow.tag_workflow.page.staff_home.staff_home.get_order_info",
-			args: {"company1": company1},
-			callback: function(r){
+			args: { "company1": company1 },
+			callback: function (r) {
 				let location = r.message.location;
 				let order = r.message.order;
 				let org_type = r.message.org_type;
 				let category = r.message.category;
-				me.update_dropdown_item(wrapper,page)
-				for (let c in category){
+				me.update_dropdown_item(wrapper, page)
+				for (let c in category) {
 					let a = document.createElement('a');
-					a.setAttribute('href',"#");
+					a.setAttribute('href', "#");
 					a.classList.add('dropdown-item')
-					a.addEventListener('click',filter_category)
+					a.addEventListener('click', filter_category)
 					a.innerHTML = category[c]
 					document.getElementById('category').appendChild(a);
 				}
@@ -69,29 +69,29 @@ frappe.StaffHome = Class.extend({
 			}
 		});
 	},
-	update_map: function(_wrapper, _page, location){
+	update_map: function (_wrapper, _page, location) {
 		let me = this;
 		let locations = location;
-		for(let c in locations) {
+		for (let c in locations) {
 			let marker = new google.maps.Marker({
 				position: new google.maps.LatLng(locations[c][1], locations[c][2]),
 				map: me.map,
-				title: locations[c][0].concat(" ",locations[c][3])
+				title: locations[c][0].concat(" ", locations[c][3])
 			});
 			console.log(marker)
 		}
 	},
-	update_order: function(_wrapper, _page, order, org_type){
-		let html = `<div style="max-height: 385px; overflow: auto;">`; 
-		for(let o in order){
+	update_order: function (_wrapper, _page, order, org_type) {
+		let html = `<div style="max-height: 385px; overflow: auto;">`;
+		for (let o in order) {
 			let from = moment(order[0].from_date)._d.toDateString();
 			let to = moment(order[0].to_date)._d.toDateString();
-			let st= order[o].job_start_time.split(':')
+			let st = order[o].job_start_time.split(':')
 			let h1 = st[0];
 			let m = st[1];
 			let h = h1 % 12 || 12;
 			let ampm = (h1 < 12 || h1 === 24) ? "AM" : "PM";
-			let timeString = h + ':'+ m +" "+ ampm;
+			let timeString = h + ':' + m + " " + ampm;
 			html += `
 				
 				<div class="row bg-white mx-2 my-3 rounded border job" data-job="${order[o].select_job}" style="margin-top: 0px !important;">
@@ -151,32 +151,32 @@ frappe.StaffHome = Class.extend({
 				</div>
 			</div>`
 		}
-		html+=`</div>`
-		let total_order = `<div class="row bg-white mx-2 my-4 rounded border" style="margin-top: 0px !important;"><div class="d-flex flex-wrap p-3" style="width: 100%;"><div class="d-flex justify-content-between w-100 "><h6 class="mb-0">Total Number Of Today's Order: </h6><h6 class="mb-0" id="counter">${order.length}</h6></div></div></div>`;
-		$("#order").html(total_order+html);
+		html += `</div>`
+		let total_order = `<div class="row bg-white mx-2 my-md-4 my-2 rounded border" style="margin-top: 0px !important;"><div class="d-flex flex-wrap p-3" style="width: 100%;"><div class="d-flex justify-content-between w-100 "><h6 class="mb-0">Total Number Of Today's Order: </h6><h6 class="mb-0" id="counter">${order.length}</h6></div></div></div>`;
+		$("#order").html(total_order + html);
 	},
-	update_dropdown_item:function(_wrapper,_page){
+	update_dropdown_item: function (_wrapper, _page) {
 		// Default Value
 		document.getElementById('category').innerHTML = '';
 		let a = document.createElement('a');
-		a.setAttribute('href',"#");
-		a.setAttribute('class','dropdown-item')
-		a.addEventListener('click',filter_category)
+		a.setAttribute('href', "#");
+		a.setAttribute('class', 'dropdown-item')
+		a.addEventListener('click', filter_category)
 		a.innerHTML = 'All'
 		document.getElementById('category').appendChild(a);
 		//Order BY
 		document.getElementById('order-by').innerHTML = '';
-		const ord = ['All','Non Exclusive','Exclusive']
-		for(let i of ord){
+		const ord = ['All', 'Non Exclusive', 'Exclusive']
+		for (let i of ord) {
 			let b = document.createElement('a');
-			b.setAttribute('href',"#");
-			b.setAttribute('class','dropdown-item');
-			b.addEventListener('click',order_by);
+			b.setAttribute('href', "#");
+			b.setAttribute('class', 'dropdown-item');
+			b.addEventListener('click', order_by);
 			b.innerHTML = i;
 			document.getElementById('order-by').appendChild(b)
 		}
 	},
-	render_map:function(r,wrapper,page){
+	render_map: function (r, wrapper, page) {
 		let me = this;
 		let location = r.message.location;
 		let order = r.message.order;
@@ -189,14 +189,14 @@ frappe.StaffHome = Class.extend({
 });
 
 
-function show_info_order(order){
+function show_info_order(order) {
 	frappe.call({
 		"method": "tag_workflow.tag_workflow.page.staff_home.staff_home.order_info",
-		"args": {"name": order},
-		"callback": function(r){
+		"args": { "name": order },
+		"callback": function (r) {
 			let data = r.message || [];
 			let html = ``;
-			for(let d in data){
+			for (let d in data) {
 				let from = moment(data[d].from_date)._d.toDateString();
 				let to = moment(data[d].to_date)._d.toDateString();
 
@@ -285,43 +285,43 @@ function show_info_order(order){
 				`;
 			}
 
-			let fields = [{"fieldname": "html", "fieldtype": "HTML", "options": html}];
-			let dialog = new frappe.ui.Dialog({title: "Quick Info",	fields: fields});
+			let fields = [{ "fieldname": "html", "fieldtype": "HTML", "options": html }];
+			let dialog = new frappe.ui.Dialog({ title: "Quick Info", fields: fields });
 			dialog.show();
 			dialog.$wrapper.find('.modal-dialog').css('max-width', '980px');
 		}
 	});
 }
 
-function redirect_order(name){
+function redirect_order(name) {
 	frappe.set_route("app", "job-order", name);
 }
 
-function redirect_doc(name){
-	location.href = '/app/'+name
+function redirect_doc(name) {
+	location.href = '/app/' + name
 }
 function filterOrder() {
-    let input, filter, o, j, a, i;
-    input = document.getElementById("my-input");
-    filter = input.value.toUpperCase();
-    o = document.getElementById("order");
-    j = o.getElementsByClassName("job");
-    for (i = 0; i < j.length; i++) {
-        a = j[i].getAttribute("data-job");
-        if (a.toUpperCase().indexOf(filter) > -1) {
-            j[i].style.display = "";
-        } else {
-            j[i].style.display = "none";
-        }
-    }
-   document.getElementById('counter').innerHTML= document.querySelectorAll('.job:not([style*="display: none"])').length;
+	let input, filter, o, j, a, i;
+	input = document.getElementById("my-input");
+	filter = input.value.toUpperCase();
+	o = document.getElementById("order");
+	j = o.getElementsByClassName("job");
+	for (i = 0; i < j.length; i++) {
+		a = j[i].getAttribute("data-job");
+		if (a.toUpperCase().indexOf(filter) > -1) {
+			j[i].style.display = "";
+		} else {
+			j[i].style.display = "none";
+		}
+	}
+	document.getElementById('counter').innerHTML = document.querySelectorAll('.job:not([style*="display: none"])').length;
 }
 
-function ajaxCallOrderBy(category,wrapper,page){
+function ajaxCallOrderBy(category, wrapper, page) {
 	frappe.call({
 		method: "tag_workflow.tag_workflow.page.staff_home.staff_home.filter_category",
-		args:{"company": company1,"category":category},
-		callback: function(r){
+		args: { "company": company1, "category": category },
+		callback: function (r) {
 			let location = r.message.location;
 			let order = r.message.order;
 			let org_type = r.message.org_type;
@@ -331,11 +331,11 @@ function ajaxCallOrderBy(category,wrapper,page){
 		}
 	});
 }
-function ajaxCallCategory(ob,wrapper,page){
+function ajaxCallCategory(ob, wrapper, page) {
 	frappe.call({
 		method: "tag_workflow.tag_workflow.page.staff_home.staff_home.filter_category",
-		args:{"company": company1,"order_by":ob},
-		callback: function(r){
+		args: { "company": company1, "order_by": ob },
+		callback: function (r) {
 			let location = r.message.location;
 			let order = r.message.order;
 			let org_type = r.message.org_type;
@@ -345,56 +345,56 @@ function ajaxCallCategory(ob,wrapper,page){
 		}
 	});
 }
-function order_by(){
-		if(this.tagName.toLowerCase()=='a' && this.text!="All" ){
-			localStorage.setItem('order_by',this.text)
-			let args = null
-			document.querySelector('.btn-link[data-ord="btn-ord"]').innerHTML=this.text
-			if(localStorage.getItem('category'))
-				args = {"company": company1,"category":localStorage.getItem('category'),"order_by":this.text}
-			else
-				args = {"company": company1,"order_by":this.text}
-			frappe.call({
-				method: "tag_workflow.tag_workflow.page.staff_home.staff_home.filter_category",
-				args:args,
-				callback: function(r){
-					frappe.flags.wrapper.render_map(r,cur_page.page,cur_page.page.page);
-				}
-			});
-		}else if(this.tagName.toLowerCase()=='a' && this.text=="All"){
-			document.querySelector('.btn-link[data-ord="btn-ord"]').innerHTML= this.text
-			if(localStorage.getItem('category'))
-				ajaxCallOrderBy(localStorage.getItem('category'),cur_page.page,cur_page.page.page)
-			else{
-				frappe.flags.wrapper.update_job_order(cur_page.page,cur_page.page.page);
-				localStorage.removeItem('order_by')
+function order_by() {
+	if (this.tagName.toLowerCase() == 'a' && this.text != "All") {
+		localStorage.setItem('order_by', this.text)
+		let args = null
+		document.querySelector('.btn-link[data-ord="btn-ord"]').innerHTML = this.text
+		if (localStorage.getItem('category'))
+			args = { "company": company1, "category": localStorage.getItem('category'), "order_by": this.text }
+		else
+			args = { "company": company1, "order_by": this.text }
+		frappe.call({
+			method: "tag_workflow.tag_workflow.page.staff_home.staff_home.filter_category",
+			args: args,
+			callback: function (r) {
+				frappe.flags.wrapper.render_map(r, cur_page.page, cur_page.page.page);
 			}
+		});
+	} else if (this.tagName.toLowerCase() == 'a' && this.text == "All") {
+		document.querySelector('.btn-link[data-ord="btn-ord"]').innerHTML = this.text
+		if (localStorage.getItem('category'))
+			ajaxCallOrderBy(localStorage.getItem('category'), cur_page.page, cur_page.page.page)
+		else {
+			frappe.flags.wrapper.update_job_order(cur_page.page, cur_page.page.page);
+			localStorage.removeItem('order_by')
 		}
+	}
 }
 
-function filter_category(){
-		if(this.tagName.toLowerCase()=='a' && this.text!="All" ){
-			localStorage.setItem('category',this.text)
-			let args = null
-			document.querySelector('.btn-link[data-cat="btn-cat"]').innerHTML=this.text
-			if(localStorage.getItem('order_by'))
-				args = {"company": company1,"category":this.text,"order_by":localStorage.getItem('order_by')}
-			else
-				args = {"company": company1,"category":this.text}
-			frappe.call({
-				method: "tag_workflow.tag_workflow.page.staff_home.staff_home.filter_category",
-				args: args,
-				callback: function(r){
-					frappe.flags.wrapper.render_map(r,cur_page.page,cur_page.page.page);
-				}
-			});
-		}else if(this.tagName.toLowerCase()=='a' && this.text=="All"){
-			document.querySelector('.btn-link[data-cat="btn-cat"]').innerHTML=this.text
-			if(localStorage.getItem('order_by'))
-				ajaxCallCategory(localStorage.getItem('order_by'),cur_page.page,cur_page.page.page)
-			else{
-				frappe.flags.wrapper.update_job_order(cur_page.page,cur_page.page.page);
-				localStorage.removeItem('category')
+function filter_category() {
+	if (this.tagName.toLowerCase() == 'a' && this.text != "All") {
+		localStorage.setItem('category', this.text)
+		let args = null
+		document.querySelector('.btn-link[data-cat="btn-cat"]').innerHTML = this.text
+		if (localStorage.getItem('order_by'))
+			args = { "company": company1, "category": this.text, "order_by": localStorage.getItem('order_by') }
+		else
+			args = { "company": company1, "category": this.text }
+		frappe.call({
+			method: "tag_workflow.tag_workflow.page.staff_home.staff_home.filter_category",
+			args: args,
+			callback: function (r) {
+				frappe.flags.wrapper.render_map(r, cur_page.page, cur_page.page.page);
 			}
+		});
+	} else if (this.tagName.toLowerCase() == 'a' && this.text == "All") {
+		document.querySelector('.btn-link[data-cat="btn-cat"]').innerHTML = this.text
+		if (localStorage.getItem('order_by'))
+			ajaxCallCategory(localStorage.getItem('order_by'), cur_page.page, cur_page.page.page)
+		else {
+			frappe.flags.wrapper.update_job_order(cur_page.page, cur_page.page.page);
+			localStorage.removeItem('category')
 		}
+	}
 }
