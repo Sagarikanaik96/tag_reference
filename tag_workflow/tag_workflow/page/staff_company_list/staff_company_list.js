645c28b39b67fd93ec1a74b5ff3bd9e05c84e518
gourav-sf <103016562+gourav-sf@users.noreply.github.com>
2022-12-12 12:19:56 +0530

    TG-4612:Hiring Searching and/or Filtering for Staffing by Name, City and TG-4885,TG-4886,TG-4887 (#2260)
    * TG-4612:Hiring Searching and/or Filtering for Staffing by Name, City, Industry, Radius, or Accreditation
    
    * TG-4612
    
    * TG-4612:Hiring Searching and/or Filtering for Staffing by Name, City, Industry, Radius, or Accreditation
    
    * TG-4612 Fix sonar issues

--- tag_workflow/tag_workflow/page/staff_company_list/staff_company_list.js
+++ tag_workflow/tag_workflow/page/staff_company_list/staff_company_list.js
@@ -1,4 +1,4 @@
-// this file for TG-2607
+frappe.flags.staff_home = null;
 frappe.pages['staff_company_list'].on_page_load = function (wrapper) {
 	let page = frappe.ui.make_app_page({
 		parent: wrapper,
@@ -6,6 +6,7 @@ frappe.pages['staff_company_list'].on_page_load = function (wrapper) {
 		single_column: true
 	});
 	wrapper.face_recognition = new frappe.FaceRecognition(wrapper, page);
+
 }
 frappe.FaceRecognition = Class.extend({
 	init: function (wrapper, page) {
@@ -16,65 +17,206 @@ frappe.FaceRecognition = Class.extend({
 			me.setup(wrapper, page);
 		}, 0);
 		$('h3[title = "Staff Company List"]').html("Staffing Companies");
+		frappe.flags.staff_home = me
 	},
 	setup: function (wrapper, page) {
 		let me = this;
+		this.start = 0
+		this.end = 20
+		this.filters = {}
+		this.options = []
+		this.data = null;
+		this.accreditation = [];
+		this.staff_comps = '';
+		this.page_value = [20,100,500];
 		this.body = $('<div></div>').appendTo(this.page.main);
 		$(frappe.render_template('staff_company_list', "")).appendTo(this.body);
 		me.show_profile(wrapper, page);
+		this.$paging_area =$(`
+			<div class="level-left">
+				<div class="btn-group">
+					<button type="button" class="btn btn-default btn-sm btn-paging btn-info active" data-value="20">20
+					</button>
+					<button type="button" class="btn btn-default btn-sm btn-paging" data-value="100">100
+					</button>
+					<button type="button" class="btn btn-default btn-sm btn-paging" data-value="500">500
+					</button>
+				</div>
+			</div>`)
+		$('.paging-area').append(this.$paging_area)
+		this.$paging_area.on("click", ".btn-paging", (e) => {
+			const $this = $(e.currentTarget);
+			this.end = $this.data().value;
+			this.update_paging_value();
+			$this.addClass('active')
+			this.refresh()
+		})
+		if (frappe.boot.tag.tag_user_info.company_type == 'Hiring'|| frappe.boot.tag.tag_user_info.company_type == "Exclusive Hiring") {
+			get_radius(25)
+			me.get_industries()
+			me.add_fields()
+
+		}
+
+
+
 	},
 	show_profile: function (_wrapper, _page) {
 		frappe.call({
 			method: "tag_workflow.tag_workflow.page.staff_company_list.staff_company_list.comp",
 			args: {
 				company_name: frappe.boot.tag.tag_user_info.company,
+				filters: this.filters,
+				start:this.start,
+				end:this.end
 			},
+			freeze: true,
+			freeze_message:  __("<b>Loading") + "...</b>",
 			callback: async function (r) {
 				let data = r.message;
+				
 				data.sort((a, b) => a.name.localeCompare(b.name))
-				let sorted_data =[];
+				let sorted_data = [];
 				let non_fav = [];
 				let profile_html = ``;
 				let favourite_companies = await sorted_favourite_company()
-				for(let p in data){
+				for (let p in data) {
+					
 					let check = favourite_companies.message.includes(data[p].name);
-					if(check){
+					if (check) {
 						data[p]['LikeStatus'] = true;
 						sorted_data.push(data[p]);
-						
-					}else{
+
+					} else {
 						data[p]['LikeStatus'] = false;
 						non_fav.push(data[p])
 					}
 				}
-				sorted_data.push(... non_fav)
-				for(let p in sorted_data){
-					profile_html = await sorted_favourite_companies(sorted_data[p], profile_html,frappe.boot.tag.tag_user_info.company_type);
+				sorted_data.push(...non_fav)
+				for (let p in sorted_data) {
+					profile_html = await sorted_favourite_companies(sorted_data[p], profile_html, frappe.boot.tag.tag_user_info.company_type);
 				}
 				$("#myTable").html(profile_html);
 				populate_filter();
 			}
 		})
 	},
+	add_fields: function () {
+		const field = [
+			{
+				'parent': '#company', 'name': 'company', 'type': 'Autocomplete', 'class': 'input-xs', 'placeholder': 'Company Name','options':this.staff_comps, 'handler': () => {
+					this.filters['company'] = document.getElementById('companys').value;
+					this.update_list()
+					this.refresh()
+				}
+			},
+			{
+				'parent': '#industry', 'name': 'industry', 'type': 'Select', 'class': 'input-xs', 'placeholder': 'Industry', 'options': this.options, 'handler': () => {
+					this.filters['industry'] = document.getElementById('industrys').value;
+					this.update_list()
+					this.refresh()
+				}
+			},
+			{
+				'parent': '#city', 'name': 'city', 'type': 'Data', 'class': 'input-xs ', 'placeholder': 'City', 'handler': () => {
+					this.filters['city'] = document.getElementById('citys').value;
+					this.update_list()
+					this.refresh()
+				}
+			},
+			{
+				'parent': '#rating', 'name': 'rating', 'type': 'Data', 'class': 'input-xs ', 'placeholder': 'Avg Rating', 'handler': () => {
+					this.filters['rating'] = document.getElementById('ratings').value;
+					this.update_list()
+					this.refresh()
+				}
+			},
+			{
+				'parent': '#accreditation', 'name': 'accreditation', 'type': 'Select', 'class': 'input-xs ', 'placeholder': 'Accreditations', 'options':this.accreditation, 'handler': () => {
+					this.filters['accreditation'] = document.getElementById('accreditations').value;
+					this.update_list()
+					this.refresh()
+				}
+			},
+
+
+		]
+		for (let f in field) {
+			let control = frappe.ui.form.make_control({
+				parent: field[f]['parent'],
+				df: {
+					label: '',
+					fieldname: field[f]['name'],
+					fieldtype: field[f]['type'],
+					input_class: field[f]['class'],
+					placeholder: field[f]['placeholder'],
+					options: field[f]['options'] ? field[f]['options'] : '',
+					onchange: field[f]['handler']
+				},
+				render_input: true,
+			})
+			control.$wrapper.find(".input-with-feedback").attr("id", field[f]['name'] + "s")
+		}
+		document.getElementById('ratings').setAttribute('type', 'number')
+	},
+	refresh: function () {
+		this.show_profile();
+	},
+	get_industries: function () {
+		this.options.push(null)
+		this.accreditation.push(null)
+		if (frappe.boot.tag.tag_user_info.company_type == "Hiring" ||frappe.boot.tag.tag_user_info.company_type == "Exclusive Hiring" ) {
+			frappe.call({
+				method: 'tag_workflow.tag_workflow.page.staff_company_list.staff_company_list.get_industries',
+				args: { user: frappe.session.user },
+				async: 0,
+				callback: (r) => {
+					if (r.message) {
+						for (let i in r.message[0])
+							this.options.push(r.message[0][i]);
+						for (let j in r.message[1])
+							this.accreditation.push(r.message[1][j])
+						this.staff_comps += r.message[2]
+					}
+				}
+			})
+		}
+	},
+	update_list: function(){
+		this.start = 0
+		this.end = 20
+		this.update_paging_value();
+		this.$paging_area.find(`.btn-paging[data-value="${this.end}"]`).addClass('active')
+
+	},
+	update_paging_value: function(){
+		for (let p in this.page_value)
+			this.$paging_area.find(`.btn-paging[data-value="${this.page_value[p]}"]`).removeClass('active')
+	
+	}
+
 })
 
 
-async function sorted_favourite_companies(data, profile_html,company_type) {
-		let link = data.name.split(' ').join('%');
-		let Likebtnexclusice = `<td></td>`
-		let Likebtnnonexclusice = `<td>
+async function sorted_favourite_companies(data, profile_html, company_type) {
+	let title = get_title(data.name)
+	let count = get_count(data.name)>1?'<i class="fa fa-plus" style="font-size:10px" aria-hidden="true"></i>' + parseInt(get_count(data.name)-1):"";
+	let link = data.name.split(' ').join('%');
+	let Likebtnexclusice = `<td></td>`
+	let Likebtnnonexclusice = `<td>
 		<svg ${data.LikeStatus ? "class='icon icon-sm liked'" : "class='icon icon-sm not-liked'"}cursor:pointer" onClick = setLike(this,"${link}")>
 		<use class="like-icon" href="#icon-heart"></use>
 		</svg>
 		</td>`
-		profile_html += `<tr data-name="${data.name}" class="comps">
-					${company_type === "Exclusive Hiring" ?Likebtnexclusice:Likebtnnonexclusice}
+	profile_html += `<tr data-name="${data.name}" class="comps">
+					${company_type === "Exclusive Hiring" ? Likebtnexclusice : Likebtnnonexclusice}
 					<td ><a onclick=dynamic_route("${link}")>${data.name}</a></td>
+					<td>${data.industry || ''}</td>
 					<td>${data.address || ''}</td>	
-					<td>${data.city || ''}</td>
-					<td>${data.state == null ? "" : data.state}</td>
-					<td>${data.zip == null ? "" : data.zip}</td>
-					<td>${data.average_rating == null ? "" : data.average_rating}</td>
+					<td ${data.average_rating ?'<span class="rating pr-2"><svg class="icon icon-sm star-click" data-rating="1"><use href="#icon-star"></use></svg></span>' + data.average_rating 
+					:""}</td>
+					 ${data.accreditation ? `<td><span class="staff-certificate-btn px-3 py-1"  id="${data.name} data-toggle="tooltip" data-placement="top" title="${title}">` + data.accreditation + '</span><span class="text-primary">' + count + '</span></td>'
+					:"<td></td>"}
 					<td>${data.is_blocked ? "<td></td>" : '<td><button class="btn-primary" onclick=trigger_direct_order("' + link + '")>Place Direct Order</button></td>'}</td>
 					</tr>`;
 	return profile_html;
@@ -177,25 +319,74 @@ async function get_favourites_list(company) {
 	return a
 }
 
-frappe.realtime.on('refresh_data',()=>{
-	if (localStorage.getItem('search')){
-		document.getElementById('myInput').value = '';
-		$('#myInput').keyup();
+frappe.realtime.on('refresh_data', () => {
+	if (localStorage.getItem('search')) {
+		document.getElementById('companys').value = '';
 		populate_filter();
-	}
+		$('#companys').change()
 		
+
+	}
+
 })
 
-function populate_filter(){
-	if(localStorage.getItem('search')){
-		const val  = localStorage.getItem('search');
-		document.getElementById('myInput').value = val
-		Array.from(document.querySelectorAll('.comps')).filter((el)=>{
-			if(!el.attributes['data-name'].value.toLowerCase().startsWith(val)){
-				console.log(el.attributes['data-name'].value)
-				document.querySelector(`#myTable tr[data-name="${el.attributes['data-name'].value}"]`).style.display = 'none'	
+function populate_filter() {
+	if (localStorage.getItem('search')) {
+		const val = localStorage.getItem('search');
+		document.getElementById('companys').value = val;
+		Array.from(document.querySelectorAll('.comps')).filter((el) => {
+			if (!el.attributes['data-name'].value.toLowerCase().startsWith(val)) {
+				document.querySelector(`#myTable tr[data-name="${el.attributes['data-name'].value}"]`).style.display = 'none'
 			}
 		})
 		localStorage.removeItem('search')
 	}
+}
+
+function get_radius(val) {
+	if (!['', undefined].includes(val)) {
+		document.getElementById('dropdownMenuLink').innerText = val
+		frappe.flags.staff_home.filters['radius'] = val
+		frappe.flags.staff_home.refresh()
+	}
+	else {
+		document.getElementById('dropdownMenuLink').innerText = 'Distance'
+		frappe.flags.staff_home.filters['radius'] = ''
+		frappe.flags.staff_home.refresh()
+	}
+
+}
+
+function get_count (company) {
+	let count =undefined
+		frappe.call({
+			method: 'tag_workflow.tag_workflow.page.staff_company_list.staff_company_list.get_count',
+			args: { company:company},
+			async: 0,
+			callback: (r) => {
+				if (r.message) {
+				console.log(r.message)
+				count = r.message
+				}
+			}
+		})
+		return count
+}
+
+
+function get_title(company){
+	let title =''
+		frappe.call({
+			method: 'tag_workflow.tag_workflow.page.staff_company_list.staff_company_list.get_title',
+			args: { company:company},
+			async: 0,
+			callback: (r) => {
+				if (r.message) {
+				 for (let i in r.message)
+				 title+= '&#x2022'+" "+r.message[i]+"\n" 
+				
+				}
+			}
+		})
+		return title
 }
--- tag_workflow/tag_workflow/page/staff_company_list/staff_company_list.py
+++ tag_workflow/tag_workflow/page/staff_company_list/staff_company_list.py
@@ -1,9 +1,12 @@
 from time import process_time
 import frappe
+import json
+from haversine import haversine, Unit
+from tag_workflow.utils.reportview import get_lat_lng,get_custom_location
 
 # this file for TG-2607
 @frappe.whitelist()
-def comp(comp_id=None,company_name=None):
+def comp(comp_id=None,company_name=None,filters=None,start=0,end=20):
     user_name = frappe.session.user
     try:
         if user_name == 'Administrator':
@@ -17,17 +20,8 @@ def comp(comp_id=None,company_name=None):
             sql = ''' select organization_type from `tabUser` where email='{}' '''.format(user_name)
             user_type=frappe.db.sql(sql, as_list=1)
             if user_type[0][0]=='Hiring':
-                sql = ''' select company from `tabUser` where email='{}' '''.format(user_name)
-                user_comp = frappe.db.sql(sql, as_list=1)
+               data = hiring_data(filters,user_name,comp_id,start,end)
 
-                sql = """  select * from `tabCompany` where name in (select parent from `tabIndustry Types` where parent in (select name from `tabCompany` where organization_type='Staffing') and industry_type in (select industry_type  from `tabIndustry Types` where parent='{}')) """.format(user_comp[0][0])
-                data = frappe.db.sql(sql, as_dict=True)
-                data = check_blocked_staffing(user_name=user_name, data=data)
-                
-                if comp_id:
-                    data=frappe.db.sql(sql, as_list=True)
-                    company_name=data[int(comp_id)-1][0]
-                    return company_data(company_name)
             elif(user_type[0][0]=='Exclusive Hiring'): 
                 sql=""" select parent_staffing from `tabCompany` where name='{0}'  """.format(company_name)
                 staff_comp_name = frappe.db.sql(sql, as_list=1)
@@ -123,3 +117,108 @@ def checking_favourites_list(company_to_favourite,user_name):
       frappe.log_error(e, "company checkig")
       frappe.msgprint("Company favourites checking")
       return "False" 
+
+
+@frappe.whitelist()
+def get_industries(user):
+    try:
+        sql  = """ select distinct(industry_type) from `tabJob Titles` where parent in (select assign_multiple_company from `tabCompanies Assigned` where parent="{0}") """.format(user)
+        industries = frappe.db.sql(sql, as_dict=True)
+        accreditation = frappe.db.sql(""" select attribute from `tabCertificate and Endorsement` """,as_dict=1)
+        accreditation = [acr['attribute'] for acr in accreditation]
+        data = [ind['industry_type'] for ind in industries]
+        comps = frappe.db.sql(""" select name from `tabCompany` where organization_type="Staffing" """,as_dict=1)
+        comps = [c['name'] for c in comps]
+        comps = '\n'.join(comps)
+        return data,accreditation,comps
+    except Exception as e:
+        frappe.log_error(e, "Industries error")
+        print(e)
+
+def get_conditions(filters):
+    cond = cond1= cond2= ''
+    if filters.get('company',None) not in [None,""]:
+        cond1 += """ and name like '%{company}%' """.format(company=filters.get('company'))
+    if filters.get('industry',None) not in [None,""]:
+        cond += """ and industry="{industry}" """.format(industry=filters.get('industry'))
+    if filters.get('city',None) not in [None,""]:
+        cond1 += """ and city like '%{0}%' """.format(filters.get('city'))
+    if filters.get('rating',None) not in [None,""]:
+        cond1 += """ and average_rating >={rating} """.format(rating=filters.get('rating'))
+    if filters.get('accreditation',None) not in [None,""]:
+        cond2 += """  and ce.certificate_type="{accreditation}" """.format(accreditation=filters.get('accreditation'))
+    return cond,cond1,cond2
+
+def filter_location(radius,comp,data):
+    try:
+        staff_location = None
+        address = frappe.db.get_value('Company',{'name':comp[0][0]},'address') or None
+        if address:
+            location = get_custom_location(address)
+            for d in data:
+                if d.address:
+                    staff_location = get_custom_location(d.address)
+                try:
+                    rad = haversine(location, staff_location, unit='mi')
+                    if rad>radius:
+                        data.remove(d)
+                except Exception:
+                    continue
+        return data
+    except Exception as e:
+        print(e)
+
+def get_custom_location(address):
+    lat, lng = get_lat_lng(address)
+    return tuple([lat, lng])
+
+def hiring_data(filters,user_name,comp_id,start,end):
+    cond=cond1 =cond2 = ''
+    try:
+        if filters:
+            filters = json.loads(filters)
+            cond,cond1,cond2 = get_conditions(filters)
+        sql = ''' select company from `tabUser` where email='{}' '''.format(user_name)
+        user_comp = frappe.db.sql(sql, as_list=1)
+        
+        sql = """  select c.*,count(ce.name) as count,ce.certificate_type as accreditation from `tabCompany` c 
+        left join `tabCertificate and Endorsement Details` ce 
+        on c.name = ce.company 
+        where c.name in (select parent from `tabIndustry Types` where parent in (select name from `tabCompany` where organization_type='Staffing' {2}) 
+        and industry_type in (select industry_type  from `tabIndustry Types` where parent='{0}'  ))  {1}  {3}   group by c.name limit {4},{5}
+        """.format(user_comp[0][0],cond,cond1,cond2,start,end)
+       
+        data = frappe.db.sql(sql, as_dict=True)
+        data = check_blocked_staffing(user_name=user_name, data=data)
+        if filters.get('radius',None):
+            radius = filters.get('radius')
+            data = filter_location(radius,user_comp,data)
+        
+        if comp_id:
+            data=frappe.db.sql(sql, as_list=True)
+            company_name=data[int(comp_id)-1][0]
+            return company_data(company_name)
+            
+        return data
+    except Exception as e:
+        print(e)
+
+@frappe.whitelist()
+def get_count(company):
+    try:
+        data = frappe.db.sql(""" select count(*) as count from `tabCertificate and Endorsement Details` where company="{0}" """.format(company),as_dict=1)
+        if len(data):
+            return data[0]['count']
+        return 0
+    except Exception as e:
+        print(e)
+
+@frappe.whitelist()
+def get_title(company):
+    try:
+        data = frappe.db.sql(""" select certificate_type as type from `tabCertificate and Endorsement Details` where company="{0}" """.format(company),as_dict=1)
+        title = [d['type'] for d in data]
+        print(title)
+        return title
+    except Exception as e:
+        print(e)
