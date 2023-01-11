5250126c8a80f5876c9e89d8cee9fec91d8ec65a
SahilMudaliar <100561494+SahilMudaliar@users.noreply.github.com>
2023-01-11 13:12:38 +0530

    TG-5537:v14: Payroll > Payroll entry(S25) (#2405)
    * TG-5532,TG-5533,TG-5536:v14: Payroll > Setup(S25fix)
    
    * TG-5537:v14: Payroll > Payroll entry(S25)

--- tag_workflow/public/js/controllers/payroll_entry.js
+++ tag_workflow/public/js/controllers/payroll_entry.js
@@ -1,10 +1,8 @@
 frappe.ui.form.on('Payroll Entry',{
     refresh:(frm)=>{
         check_payroll_perm()
-        setTimeout(()=>{
-            check_status(frm)
-        },200)
-        
+        check_status(frm)
+        frm.set_value("company",(frappe.boot.tag.tag_user_info.comps.length==1) ? frappe.boot.tag.tag_user_info.company :"")
     },
     setup:function(frm){
 		frm.set_query("company", function() {
@@ -16,5 +14,6 @@ frappe.ui.form.on('Payroll Entry',{
                 ]
 			}
 		});
+
 	},
 })
