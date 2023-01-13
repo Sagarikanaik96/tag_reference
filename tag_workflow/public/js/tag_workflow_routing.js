99d91d620a88b8cbcd9b3e1099fb3dd74223544f
Sahil Mudaliar <sahil.mudaliar@sourcefuse.com>
2023-01-13 13:35:40 +0530

    TG-5647:V-14-> Getting page not found pop-up when the user clicks on the back button from the listing pages(S25Fix)

--- tag_workflow/public/js/tag_workflow_routing.js
+++ tag_workflow/public/js/tag_workflow_routing.js
@@ -2,7 +2,7 @@ $(document).on("startup", function () {
 	let url=window.location.href
 	let url_splits = url.split("home", 2);
 	if(url_splits.length==2){
-		if(url_splits[1]!='/' && url_splits[1]!=''){
+		if(url_splits[1]!='/' && url_splits[1]!='' && url_splits[1]!="#"){
 			frappe.throw('Page '+decodeURI(url_splits[1])+' not found')
 		}
 	}
